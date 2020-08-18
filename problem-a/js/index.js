c6d92a633ddb77b997820751e3b18687db68b5d3
Siddhant Jain <jainsid@uw.edu>
2020-08-17 17:47:28 -0700

    Finish 7a, have few jest issues that I can't solve

--- problem-a/js/index.js
+++ problem-a/js/index.js
@@ -2,7 +2,118 @@
 
 /* your code goes here! */
 
+class Task {
 
+  constructor(newDescString, newIsComplete){
+    this.descString = newDescString;
+    this.isComplete = newIsComplete;
+  }
+
+  render() {
+    let elem = document.createElement('li');
+    elem.textContent = this.descString;
+
+    if(this.isComplete){
+      elem.classList.add('font-strike');
+    }
+
+    elem.addEventListener('click', () => {
+      this.toggleFinished();
+      elem.classList.toggle('font-strike');
+    })
+    
+    return elem;
+  }
+
+  toggleFinished() {
+    this.isComplete = !this.isComplete;
+  }
+}
+
+class TaskList {
+
+  constructor(taskArray){
+    this.tasks = taskArray;
+  }
+
+  addTask(description) {
+    let newTask = new Task(description, false);
+    this.tasks.push(newTask);
+  }
+
+  render(){
+    let olElem = document.createElement('ol');
+    this.tasks.forEach((taskObj) => {
+      let taskElem = taskObj.render();
+      olElem.appendChild(taskElem);
+    })
+    return olElem;
+  }
+}
+
+class NewTaskForm {
+
+  constructor(funcWhenSubmitted) {
+    this.submitCallback = funcWhenSubmitted;
+  }
+
+  render() {
+    let formElem = document.createElement('form');
+
+    let inputElem = document.createElement('input');
+    inputElem.classList.add('form-control', 'mb-3');
+    inputElem.setAttribute('placeholder', "What else do you have to do?");
+    formElem.appendChild(inputElem);
+
+    let btnElem = document.createElement('button');
+    btnElem.classList.add('btn', 'btn-primary');
+    btnElem.textContent = "Add task to list";
+    formElem.appendChild(btnElem);
+
+    btnElem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      let inputValue = inputElem.value;
+
+      let whatToDo = this.submitCallback;
+      whatToDo(inputValue);
+    })
+
+    return formElem;
+  }
+}
+
+class App {
+
+  constructor(parent, list) {
+    this.parentElem = parent;
+    this.taskList = list;
+  }
+
+  render() {
+    let listElem = this.taskList.render();
+    this.parentElem.appendChild(listElem);
+    let whichToCall = (arg) => this.addTaskToList(arg);
+    let formObj = new NewTaskForm(whichToCall);
+    this.parentElem.appendChild(formObj.render());
+  }
+
+  addTaskToList(descString) {
+    this.taskList.addTask(descString);
+    this.parentElem.innerHTML = '';
+    this.render();
+  }
+}
+
+let taskListObj = new TaskList([
+  new Task('Make some classes', true),
+  new Task('Arrow some functions', false)
+]);
+
+
+let appElem = document.querySelector('#app');
+let appObj = new App(appElem, taskListObj);
+appObj.render();
 
 
 
