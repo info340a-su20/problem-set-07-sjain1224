3fd62b4ca06f62afaa8567ec7f257b4f0c23cf5e
Siddhant Jain <jainsid@uw.edu>
2020-08-17 22:03:02 -0700

    Pass Jest Problem B, but have a weird error

---
+++ problem-b/js/Controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import readline from 'readline-sync';
+
+import * as model from './Model';
+import {printTweets} from './View';
+
+export function runSearch() {
+    console.log('Here are some tweets by @UW_iSchool');
+    let recent = model.getRecentTweets();
+    printTweets(recent);
+
+    let userInput = readline.question('Search tweets, or EXIT to quit: ');
+
+    console.log('Im working');
+
+    if(userInput === 'EXIT') {
+        return;
+    }
+
+    let results = model.searchTweets(userInput);
+    printTweets(results);
+}
--- problem-b/js/index.js
+++ problem-b/js/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
-console.log("If you see this message, then the app ran successfully! Be sure to delete this line of code.")
-
 /* your code will go here! */
+import { runSearch } from './Controller';
+
+runSearch();
